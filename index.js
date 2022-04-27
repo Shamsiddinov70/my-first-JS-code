191dbb5c29e78d47a8ce1ae0e1e88a7fdd47f8ca
Shamsiddinov70 <abbosshamsiddin@gmail.com>
2022-04-27 18:42:06 +0500

    add initial commits

---
+++ index.js
@@ -0,0 +1,57 @@
+// alert ('Teshovoy Korporatsiyasiga xush kelibsiz');
+// var userName = prompt("Please enter your name", 'abbos')
+// let person = prompt("Please enter your name", 'abbos');
+// var person = prompt("Please enter your age", '21');
+// console.log(userName , person);
+
+
+// alert ('welcome to MIT university');
+// var userName = prompt("Please enter your name", 'Abdulloh');
+// var age = prompt("Please enter your age", '21');
+// var field = prompt("what is your major?", 'Computer engineering');
+
+// var userName = 'Abdulloh';
+// var age = '21';
+// var field = 'Computer engineering';
+
+
+// console.log(`
+// Student
+// Name: ${userName}
+// Age: ${age}`);
+
+
+alert('Wlecome to MIT university!');
+let userName = prompt("Please enter your name", 'Abdulloh');
+var name = userName;
+
+let age = prompt("Please enter your age", '21');
+var userAge = age;
+
+let field = prompt("what is your major?", 'Computer engineering');
+var major = field;
+
+let nationality = prompt("what is your nationality?", 'Uzbek');
+var nation = nationality;
+
+let isMarried = prompt("Are you married?", 'yes');
+var wedding = isMarried;
+
+let purpose = prompt("What is your purpose?", 'to be programmer');
+var aim = purpose;
+
+let family = prompt("How is your family's financial situation?", 'not bad');
+var financialSituation = family;
+
+
+
+console.log(`
+About student
+
+Name: ${name}
+Age: ${userAge}
+Major: ${major}
+Nationality: ${nation}
+Wedding: ${wedding}
+Purpose: ${aim}
+Family situation: ${financialSituation}`)
